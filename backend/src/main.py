5c258b0822696b34404a81f4daf1cfbf7faf937f
Amit Thakur <43364783+athakur73@users.noreply.github.com>
2021-04-22 19:32:43 +0530

    add logic to include email ID in the login page (#57)
    Issue No- 54
    
    Co-authored-by: Amit <mani120488@gmail.com>

--- backend/src/main.py
+++ backend/src/main.py
@@ -53,16 +53,20 @@ def login():
     if emp_id==None or password==None:
         return jsonify({"error:":"incorrect username or password"}), 200
     session = Session()
-    auth_object = session.query(authUser).filter(authUser.emp_id == emp_id).first()
-    if auth_object and auth_object.password==None:
-            return jsonify({"warning":"Password Not set Please set password"}), 200 
+    # we can use both emp_id and email as a user id.
+    if emp_id.find("@") != -1:
+        auth_object = session.query(authUser).filter(authUser.email == emp_id, authUser.password == password).first()
+    else:
+        auth_object = session.query(authUser).filter(authUser.emp_id == emp_id, authUser.password == password).first()
 
-    auth_object = session.query(authUser).filter(authUser.emp_id == emp_id, authUser.password == password).first()
-    if auth_object==None:
-        return jsonify({"error":"Username or password is incorrect"}), 200 
+    if auth_object and auth_object.password==None:
+        return jsonify({"warning": "Password Not set Please set password"}), 200
+
+    if auth_object is None:
+        return jsonify({"error": "Username or password is incorrect"}), 200
     emp_obj = session.query(employee).filter(employee.emp_id == emp_id).first()
     employee_name = "Full Name"
-    roles=auth_object.roles
+    roles = auth_object.roles
     login=True
     access_token = create_access_token(identity=emp_id)
     return jsonify(access_token=access_token,username=emp_id,roles=roles,login=login,employee_name = employee_name)
