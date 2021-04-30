5c6785a7edd2af642ae987b9469cd96e6138044c
Manikandan <manikandan.l@indiumsoft.com>
2021-04-30 13:28:28 +0530

    implement_search_filter

--- frontend/frontend/src/app/add-employee/add-employee.component.ts
+++ frontend/frontend/src/app/add-employee/add-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit,ViewEncapsulation } from '@angular/core';
-import {  FormGroup,FormControl } from '@angular/forms';
+import {  FormGroup,FormControl, Validators } from '@angular/forms';
 import {employee} from './employees';
 import { addEmployeeService } from './add-employee.service';
 import {CookieService} from 'ngx-cookie-service';
@@ -18,27 +18,39 @@ import {MatCalendarCellClassFunction,MatDatepickerModule} from '@angular/materia
 export class AddEmployeeComponent implements OnInit {
   durationInSeconds = 5;
   employee = new employee();
-  // profileForm = new FormGroup({
-  //   firstName: new FormControl(''),
-  //   lastname: new FormControl(''),
-  //   surname: new FormControl(''),
-  //   initial: new FormControl(''),
-  //   salutation: new FormControl(''),
-  //   managername: new FormControl(''),
-  //   managerdept: new FormControl(''),
-  //   projectid: new FormControl(''),
-  //   empstatus: new FormControl(''),
-  //   dept: new FormControl(''),
-  //   empstartdate: new FormControl(''),
-  //   emplwd: new FormControl(''),
-  //   empprojectassigneddate: new FormControl(''),
-  //   empprojectenddate: new FormControl(''),
+  emailPattern =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+  isValidFormSubmitted = false;  
+  // employee = new FormGroup({
+  //   emp_id : new FormControl(),
+  //   email : new FormControl(),
+  //   firstName:new FormControl(),
+  //   lastname:new FormControl(),
+  //   surname:new FormControl(),
+  //   initial:new FormControl(),
+  //   salutation:new FormControl(),
+  //   managername:new FormControl(),
+  //   project_code:new FormControl(),
+  //   geography:new FormControl(),
+  //   dept:new FormControl(),
+  //   designation:new FormControl(),
+  //   emp_start_date:new FormControl(),
+  //   emp_last_working_date:new FormControl(),
+  //   emp_project_assigned_date:new FormControl(),
+  //   emp_project_end_date:new FormControl(),
+  //   employment_status:new FormControl(),
+  //   manager_name:new FormControl(),
+  //   manager_dept:new FormControl(),
+  //   resource_status:new FormControl(),
+  //   delivery_type:new FormControl(),
+  //   additional_allocation:new FormControl(),
+  //   skills:new FormControl(),
+  //   roles:new FormControl()
     
   // });
   public employee_fields_list:any 
   name = new FormControl('');
   constructor(private apiService:addEmployeeService,private router: Router,private cookieService: CookieService) { }
-
+  
   ngOnInit(): void {
     if (this.cookieService.get('login')=='true'){}
     else{
@@ -46,13 +58,20 @@ export class AddEmployeeComponent implements OnInit {
     }
   }
  
-  onSubmit() {
-    
-    console.log(this.employee);
-    this.apiService.addEmployee(this.employee).subscribe(data=>{this.apiService.showMessage(Object.values(data),Object.keys(data))})
+  onSubmit(data) {
+    console.log(this.employee)
+    this.isValidFormSubmitted = false;  
+  
+    if (data.email.invalid) {  
+       return;  
+    }  
+  
+    this.isValidFormSubmitted = true;  
+    // this.apiService.addEmployee(this.employee).subscribe(data=>{this.apiService.showMessage(Object.values(data),Object.keys(data))})
    
   }
-
+  
+  
   // dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
   //   // Only highligh dates inside the month view.
   //   if (view === 'month') {
