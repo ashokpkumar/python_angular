5f0601c9966194e94e6d968f6d24c714dcbfd6a1
Amit <amitkumarthakur73@gmail.com>
2021-06-07 12:49:20 +0530

    fix database connection issue

--- backend/src/env/config.py
+++ backend/src/env/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://ADD_SQL_SERVER_NAME/master?driver=SQL Server?Trusted_Connection=yes"
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://localhost\SQLEXPRESS/master?driver=SQL Server?Trusted_Connection=yes"
     JWT_SECRET_KEY = "super-secret"
 
     MAIL_SERVER = 'smtp.gmail.com'
