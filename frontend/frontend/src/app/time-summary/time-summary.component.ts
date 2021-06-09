013ead9d5dc9d58411f3436c08ccdaf42fe4fb5a
Amit <amitkumarthakur73@gmail.com>
2021-06-09 12:57:11 +0530

    calendar dependency bug

--- frontend/frontend/src/app/time-summary/time-summary.component.ts
+++ frontend/frontend/src/app/time-summary/time-summary.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
-import {Router} from "@angular/router"
+import {Router} from "@angular/router";
+import * as $ from 'jquery';
+import 'fullcalendar';
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-time-summary',
   templateUrl: './time-summary.component.html',
@@ -8,7 +12,49 @@ import {Router} from "@angular/router"
 })
 export class TimeSummaryComponent implements OnInit {
 
-  constructor(private router: Router,private cookieService: CookieService) { }
+  @Input() eventData: any;
+   
+   defaultConfigurations: any;
+
+  @Input()
+      set configurations(config: any) {
+         if(config) {
+            this.defaultConfigurations = config;  
+         }
+      }
+
+
+  constructor(private router: Router,private cookieService: CookieService) {
+    this.defaultConfigurations = {
+      editable: true,
+               eventLimit: true,
+               titleFormat: 'MMM D YYYY',
+               header: {
+                  left: 'prev,next today',
+                  center: 'title',
+                  right: 'month,agendaWeek,agendaDay'
+               },
+               buttonText: {
+                  today: 'Today',
+                  month: 'Month',
+                  week: 'Week',
+                  day: 'Day'
+               },
+               views: {
+                  agenda: {
+                     eventLimit: 2
+                  }
+               },
+               allDaySlot: false,
+               slotDuration: moment.duration('00:15:00'),
+               slotLabelInterval: moment.duration('01:00:00'),
+               firstDay: 1,
+               selectable: true,
+               selectHelper: true,
+               events: this.eventData,
+            };
+         
+   }
 
   ngOnInit(): void {
     if (this.cookieService.get('login')=='true'){}
