b56463a6cb6698a2a3583e00049f22f01e9282d2
Manikandan <manikandan.l@indiumsoft.com>
2021-06-15 11:38:12 +0530

    fullCalenderView

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs'
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs'
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -10,12 +11,18 @@ import {Subscription} from 'rxjs'
 export class AppComponent implements OnInit, OnDestroy {
   title = 'frontend';
   examsListSubs: Subscription;
-  
-  
-  ngOnInit() {
-  
+
+  currentRoute: string;
+  constructor(private router: ActivatedRoute) {
+
   }
 
+  ngAfterViewChecked() {
+    this.currentRoute = this.router['_routerState']['snapshot']['url']
+  }
+  ngOnInit() {
+    this.currentRoute = this.router['_routerState']['snapshot']['url']
+  }
   ngOnDestroy() {
     this.examsListSubs.unsubscribe();
   }
--- frontend/frontend/src/app/time-summary/time-summary.component.ts
+++ frontend/frontend/src/app/time-summary/time-summary.component.ts
@@ -1,66 +1,67 @@
-import { Component, OnInit, Input  } from '@angular/core';
-import {CookieService} from 'ngx-cookie-service';
-import {Router} from "@angular/router";
-import * as $ from 'jquery';
-import 'fullcalendar';
+import { Component, OnInit, Input } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from "@angular/router";
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 import * as moment from 'moment';
 
 @Component({
-  selector: 'app-time-summary',
-  templateUrl: './time-summary.component.html',
-  styleUrls: ['./time-summary.component.css']
+   selector: 'app-time-summary',
+   templateUrl: './time-summary.component.html',
+   styleUrls: ['./time-summary.component.css']
 })
 export class TimeSummaryComponent implements OnInit {
 
-  @Input() eventData: any;
-   
+   @Input() eventData: any;
+
    defaultConfigurations: any;
 
-  @Input()
-      set configurations(config: any) {
-         if(config) {
-            this.defaultConfigurations = config;  
-         }
+   @Input()
+   set configurations(config: any) {
+      if (config) {
+         this.defaultConfigurations = config;
       }
-
-
-  constructor(private router: Router,private cookieService: CookieService) {
-    this.defaultConfigurations = {
-      editable: true,
-               eventLimit: true,
-               titleFormat: 'MMM D YYYY',
-               header: {
-                  left: 'prev,next today',
-                  center: 'title',
-                  right: 'month,agendaWeek,agendaDay'
-               },
-               buttonText: {
-                  today: 'Today',
-                  month: 'Month',
-                  week: 'Week',
-                  day: 'Day'
-               },
-               views: {
-                  agenda: {
-                     eventLimit: 2
-                  }
-               },
-               allDaySlot: false,
-               slotDuration: moment.duration('00:15:00'),
-               slotLabelInterval: moment.duration('01:00:00'),
-               firstDay: 1,
-               selectable: true,
-               selectHelper: true,
-               events: this.eventData,
-            };
-         
    }
 
-  ngOnInit(): void {
-    if (this.cookieService.get('login')=='true'){}
-    else{
-      this.router.navigate(['/login']);
-    }
-  }
+   calendarOptions: CalendarOptions = {
+      initialView: 'dayGridMonth'
+   };
+   constructor(private router: Router, private cookieService: CookieService) {
+      // this.defaultConfigurations = {
+      //    editable: true,
+      //    eventLimit: true,
+      //    titleFormat: 'MMM D YYYY',
+      //    header: {
+      //       left: 'prev,next today',
+      //       center: 'title',
+      //       right: 'month,agendaWeek,agendaDay'
+      //    },
+      //    buttonText: {
+      //       today: 'Today',
+      //       month: 'Month',
+      //       week: 'Week',
+      //       day: 'Day'
+      //    },
+      //    views: {
+      //       agenda: {
+      //          eventLimit: 2
+      //       }
+      //    },
+      //    allDaySlot: false,
+      //    slotDuration: moment.duration('00:15:00'),
+      //    slotLabelInterval: moment.duration('01:00:00'),
+      //    firstDay: 1,
+      //    selectable: true,
+      //    selectHelper: true,
+      //    events: this.eventData,
+      // };
+
+   }
+
+   ngOnInit(): void {
+      if (this.cookieService.get('login') == 'true') { }
+      else {
+         this.router.navigate(['/login']);
+      }
+   }
 
 }
