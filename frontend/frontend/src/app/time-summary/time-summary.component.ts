a50be8bd0a4239d8a45ce1afe2ea5e54164fa653
Amit <amitkumarthakur73@gmail.com>
2021-06-16 19:57:38 +0530

    Code update

--- frontend/frontend/src/app/time-summary/time-summary.component.ts
+++ frontend/frontend/src/app/time-summary/time-summary.component.ts
@@ -10,11 +10,8 @@ import * as moment from 'moment';
    styleUrls: ['./time-summary.component.css']
 })
 export class TimeSummaryComponent implements OnInit {
-
    @Input() eventData: any;
-
    defaultConfigurations: any;
-
    @Input()
    set configurations(config: any) {
       if (config) {
@@ -23,40 +20,16 @@ export class TimeSummaryComponent implements OnInit {
    }
 
    calendarOptions: CalendarOptions = {
-      initialView: 'dayGridMonth'
+      initialView: 'dayGridMonth',  
+      dateClick: this.onDateClick.bind(this),
+      
    };
-   constructor(private router: Router, private cookieService: CookieService) {
-      // this.defaultConfigurations = {
-      //    editable: true,
-      //    eventLimit: true,
-      //    titleFormat: 'MMM D YYYY',
-      //    header: {
-      //       left: 'prev,next today',
-      //       center: 'title',
-      //       right: 'month,agendaWeek,agendaDay'
-      //    },
-      //    buttonText: {
-      //       today: 'Today',
-      //       month: 'Month',
-      //       week: 'Week',
-      //       day: 'Day'
-      //    },
-      //    views: {
-      //       agenda: {
-      //          eventLimit: 2
-      //       }
-      //    },
-      //    allDaySlot: false,
-      //    slotDuration: moment.duration('00:15:00'),
-      //    slotLabelInterval: moment.duration('01:00:00'),
-      //    firstDay: 1,
-      //    selectable: true,
-      //    selectHelper: true,
-      //    events: this.eventData,
-      // };
+   
+    onDateClick(res) {
+      alert('Clicked on date : ' + res.dateStr)
+    }
 
-   }
-
+   constructor(private router: Router, private cookieService: CookieService) {}
    ngOnInit(): void {
       if (this.cookieService.get('login') == 'true') { }
       else {
