30c7dbcc1f2478d99ac4b6652bf109c5b84c717f
Ashok kumar <mani120488@gmail.com>
2021-08-09 00:21:13 +0530

    added Blue print
    added Blue print

---
+++ backend/src/entities/modules/employee.py
@@ -0,0 +1,123 @@
+from flask import Blueprint, jsonify, request
+import datetime
+from entities.database import employee,project,authUser,timesubmissions
+from entities.database import Session
+from entities.database import serialize_all
+
+employee_module = Blueprint(name="employee", import_name=__name__)
+
+@employee_module.route('/employees')
+def employees():
+    session = Session()
+    emp_objects = session.query(employee).all()
+    serialized_obj = serialize_all(emp_objects)
+    serialed_out = []
+    for dictionary in serialized_obj:
+        dictionary['full_name'] = dictionary['first_name'] + dictionary['last_name']
+        serialed_out.append(dictionary)
+
+    project_objects = session.query(project).all()
+    serialized_project = serialize_all(project_objects)
+    for dictionary in serialed_out:
+        emp_id = dictionary["project_code"]
+        for proj_item in serialized_project:
+            if proj_item["project_code"]==emp_id:
+                proj_item["project_code"] = [emp_id] #Project id should return list instead of string in employee
+                dictionary.update(proj_item)
+                break
+    session.close()
+    return (jsonify(serialed_out))
+
+@employee_module.route('/addEmployee', methods=['POST'])
+def addEmployee():
+    data = request.get_json()
+    emp_id=data.get("emp_id")
+    email=data.get("email")
+    project_code=data.get("project_code")
+    session = Session()
+    existing_emp = session.query(employee).filter(employee.emp_id==emp_id).first()
+    if existing_emp:
+        session.close()
+        return jsonify({'warning':'User ID: {} already exist !'.format(emp_id)})
+    #print(emp_id)
+    existing_emp = session.query(employee).filter(employee.email==email).first()
+    if existing_emp:
+        session.close()
+        return jsonify({'warning':'Email ID: {} already exist !'.format(email)})
+    existing_project = session.query(project).filter(project.project_code==project_code).first()
+    if existing_project==None:
+        return jsonify({'error':'Project with ID: {} Does not Exist !'.format(project_code)})
+
+    try: 
+        emp_data = employee(emp_id=data.get("emp_id").lower() , 
+                            manager_id = data.get("manager_id"),
+                            email = data.get("email").lower(), 
+                            first_name = data.get("first_name").lower(), 
+                            last_name= data.get("last_name").lower(), 
+                            sur_name= data.get("sur_name").lower(), 
+                            initial= data.get("initial").lower(), 
+                            salutation= data.get("salutation").lower(), 
+                            project_code= data.get("project_code").lower(), 
+                            dept= data.get("dept").lower(),
+                            designation = data.get("designation").lower(),
+                            emp_start_date= data.get("emp_start_date",None),
+                            emp_last_working_date=data.get("emp_last_date",None),
+                            emp_project_assigned_date=data.get("emp_project_assigned_date",None),
+                            emp_project_end_date=data.get("emp_project_end",None),
+
+                            employment_status=data.get("employment_status").lower(), 
+                            manager_name=data.get("manager_name").lower(), 
+                            manager_dept=data.get("manager_dept").lower(), 
+                            resource_status=data.get("resource_status").lower(),
+                            delivery_type=data.get("delivery_type").lower(),
+                            additional_allocation=data.get("additional_allocation").lower(),
+                            skills=data.get("skills").lower(),
+                            roles=data.get("roles").lower(),
+
+                            )
+        session = Session()
+        session.add(emp_data)
+        session.commit()
+        auth_data = authUser(emp_id = data.get("emp_id").lower(),
+                            email=data.get("email").lower(),
+                            roles=data.get("roles").lower())
+        session = Session()
+        session.add(auth_data)
+        session.commit()     
+        return jsonify({"success":"successfully added employee {}".format(data.get("emp_id"))}),200     
+    except:
+        return jsonify({"error":"Some error happened in adding the employee"}),500
+
+@employee_module.route("/viewEmpInfo", methods=["POST"])
+def viewEmpInfo():
+    emp_id = request.json.get("emp_id", None)
+    # print("????????????")
+    # print(emp_id)
+    if emp_id is None:
+        return jsonify({"error":"Employee ID is empty"}), 201
+    session = Session()
+    # print(emp_id)
+    emp_objects = session.query(employee).filter(employee.emp_id==emp_id).all()
+    # print(">>>>>>>>>>>>")
+    # print(emp_objects)
+    if emp_objects == []:
+        return jsonify({"error":"Employee Not found !"}), 201
+    emp_serialized = serialize_all(emp_objects)
+    # print(emp_serialized)
+    emp_dict = emp_serialized[0]
+    project_objects = session.query(project).filter(project.project_code==emp_serialized[0]['project_code']).all()
+    if project_objects:
+        project_serialized = serialize_all(project_objects)
+        proj_dict = project_serialized[0]
+        emp_dict.update(proj_dict)
+    
+    first_name=session.query(employee.first_name).filter(employee.emp_id==emp_id).first()[0]
+    last_name=session.query(employee.last_name).filter(employee.emp_id==emp_id).first()[0]                
+    initial=session.query(employee.initial).filter(employee.emp_id==emp_id  ).first()[0]
+    first_name1=first_name.capitalize()
+    initial1=initial.upper()
+    user_name = (first_name1+" "+last_name +"."+ initial1)
+    emp_dict['full_name'] = user_name
+    return jsonify(emp_dict), 201
+
+
