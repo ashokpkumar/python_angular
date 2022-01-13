8e81dda956c96baf27c2e486f33458f68af95af0
Ashok kumar <mani120488@gmail.com>
2022-01-13 13:50:17 +0530

    changed the DB to mysql
    changed the DB to mysql

--- backend/src/entities/database.py
+++ backend/src/entities/database.py
@@ -6,8 +6,8 @@ import pyodbc
 from sqlalchemy.orm.attributes import QueryableAttribute
 from env.config import Config
 
-
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI,pool_size=10, max_overflow=20)
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{Config.USER_DB}:{Config.PWD_DB}@{Config.HOST_DB}:{Config.PORT_DB}/{Config.NAME_DB}'
+engine = create_engine(SQLALCHEMY_DATABASE_URI,pool_size=10, max_overflow=20)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
@@ -15,76 +15,72 @@ Base = declarative_base()
 class authUser(Base):
     __tablename__ = 'authUser'
     id = Column(Integer, primary_key=True)
-    emp_id = Column(String)
-    email = Column(String)
-    password = Column(String)
+    emp_id = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(50))
     roles = Column(VARCHAR(2000))
 
 class timesubmissions(Base):
     __tablename__="timesubmissions"
     id = Column(Integer, primary_key=True)
     date_info = Column(DateTime)
-    user_id = Column(String)
-    manager_id = Column(String)
-    time_type = Column(String)
-    submission_id = Column(String)
-    status = Column(String) # ['submitted-pending approval','approved','unapproved']
+    user_id = Column(String(50))
+    manager_id = Column(String(50))
+    time_type = Column(String(50))
+    submission_id = Column(String(50))
+    status = Column(String(50)) # ['submitted-pending approval','approved','unapproved']
     hours = Column(Integer)
-    project_code=Column(String)
-    task_id=Column(String)
-    description=Column(String)
-    remarks=Column(String)
+    project_code=Column(String(50))
+    task_id=Column(String(50))
+    description=Column(String(1000))
+    remarks=Column(String(1000))
 
 class designation(Base):
     __tablename__="designation"
     id=Column(Integer,primary_key=True)
-    designation=Column(String)
+    designation=Column(String(50))
 
 class department(Base):
     __tablename__="department"
     id=Column(Integer,primary_key=True)
-    department_name=Column(String)
+    department_name=Column(String(50))
 
 class announcements(Base):
     __tablename__="announcements"
     id = Column(Integer, primary_key=True)
-    user_id = Column(String)
-    announcement_info = Column(String)
-    announcement_category = Column(String)
+    user_id = Column(String(50))
+    announcement_info = Column(String(50))
+    announcement_category = Column(String(50))
     date_logged = Column(DateTime)
     
 class employee(Base):
     __tablename__ = 'employee'
     id = Column(Integer, primary_key=True)
-    emp_id = Column(String)
-    manager_id = Column(String)
-    email = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
-    sur_name = Column(String)
-    initial = Column(String) 
-    salutation = Column(String) #mr,ms, Miss, Mrs
-    project_code = Column(VARCHAR(2000))
-    dept = Column(String)
-    designation = Column(String)
+    emp_id = Column(String(50))
+    manager_id = Column(String(50))
+    email = Column(String(50))
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    sur_name = Column(String(50))
+    initial = Column(String(50)) 
+    salutation = Column(String(5)) #mr,ms, Miss, Mrs
+    project_code = Column(VARCHAR(50))
+    dept = Column(String(50))
+    designation = Column(String(50))
     
-    emp_start_date = Column(VARCHAR(2000))
-    emp_last_working_date = Column(VARCHAR(2000))
-    emp_project_assigned_date = Column(VARCHAR(2000))
-    emp_project_end_date = Column(VARCHAR(2000))
+    emp_start_date = Column(VARCHAR(50))
+    emp_last_working_date = Column(VARCHAR(50))
+    emp_project_assigned_date = Column(VARCHAR(50))
+    emp_project_end_date = Column(VARCHAR(50))
 
-    employment_status = Column(String) #in project, new joined, in notice, bench, relieved
-    manager_name = Column(String) 
-    manager_dept = Column(String)
-    resource_status = Column(String)
-    delivery_type = Column(String)
-    additional_allocation =  Column(String)
-    skills =  Column(String)
+    employment_status = Column(String(50)) #in project, new joined, in notice, bench, relieved
+    manager_name = Column(String(50)) 
+    manager_dept = Column(String(50))
+    resource_status = Column(String(50))
+    delivery_type = Column(String(50))
+    additional_allocation =  Column(String(50))
+    skills =  Column(String(50))
     roles =  Column(VARCHAR(2000))
-    # def __init__(self, created_by):
-    #     self.created_at = datetime.now()
-    #     self.updated_at = datetime.now()
-    #     self.last_updated_by = created_by
 
     
     def to_dict(self, show=None, _hide=[], _path=None):
@@ -199,18 +195,18 @@ class employee(Base):
 class project(Base):
     __tablename__ = 'project'
     id = Column(Integer, primary_key=True)
-    client_name = Column(String)
-    project_code = Column(String)
-    project_name = Column(String)
-    project_manager_id=Column(VARCHAR(2000))
-    project_start_date = Column(VARCHAR(2000))
-    project_status = Column(String)
-    billing_type = Column(String)
-    segment = Column(String)
-    geography = Column(String)
-    solution_category = Column(String)
-    financial_year = Column(String)
-    resource_info = Column(String)
+    client_name = Column(String(50))
+    project_code = Column(String(50))
+    project_name = Column(String(50))
+    project_manager_id=Column(VARCHAR(50))
+    project_start_date = Column(VARCHAR(50))
+    project_status = Column(String(50))
+    billing_type = Column(String(50))
+    segment = Column(String(50))
+    geography = Column(String(50))
+    solution_category = Column(String(50))
+    financial_year = Column(String(50))
+    resource_info = Column(String(50))
 
     def to_dict(self, show=None, _hide=[], _path=None):
         """Return a dictionary representation of this model."""
@@ -320,28 +316,12 @@ class project(Base):
                         pass
 
         return ret_data
-    #serialize_only = ('id', 'email_id', 'role_type', 'users.id')
-    # _default_fields = [
-    #     "title",
-    #     "description",
-    # ]
-    # _hidden_fields = []
-    # _readonly_fields = []
-
-    # def __init__(self, title, description, created_by):
-    #     Entity.__init__(self, created_by)
-    #     self.title = title
-    #     self.description = description
+  
 def serialize_all(data_obj):
     serialized = []
     for data in data_obj:
         data_dict = data.__dict__
-        #print(data_dict)
-    
         data_dict_keys = list(data_dict.keys())
-        #print(data_dict_keys)
-        #print(type(data_dict_keys))
-
         serial_dict ={}
         for i in range(1,len(data_dict_keys)):
             serial_dict[data_dict_keys[i]]=data_dict[data_dict_keys[i]]
@@ -352,16 +332,16 @@ def serialize_all(data_obj):
 class TimeMaster(Base):
     __tablename__ = 'TimeMaster'
     id = Column(Integer, primary_key=True)
-    emp_id = Column(String)
-    month = Column(String)
-    year = Column(String)
+    emp_id = Column(String(50))
+    month = Column(String(50))
+    year = Column(String(50))
     timedata = Column(VARCHAR(3000))
 
 class forget_pass(Base):
     __tablename__ = 'forget_pass'
     id = Column(Integer, primary_key=True)
-    user_id = Column(String)
-    email_address = Column(String)
-    reset_token = Column(String)
+    user_id = Column(String(50))
+    email_address = Column(String(50))
+    reset_token = Column(String(50))
     create_date = Column(VARCHAR(2000))
 
--- backend/src/env/config.py
+++ backend/src/env/config.py
@@ -6,7 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://localhost\SQLEXPRESS/master?driver=SQL Server?Trusted_Connection=yes"
+    #SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://localhost\SQLEXPRESS/master?driver=SQL Server?Trusted_Connection=yes"
+    
     JWT_SECRET_KEY = "super-secret"
     FERNET_KEY=b'cx9VhUbmHbJMrVBtSL4Isusu7aN1lJ25bMPlS2jefJE='
 
@@ -23,33 +24,9 @@ class Config:
     AUTH_TOKEN_EXPIRY_SECS = 60
     FERNET_KEY = b'cx9VhUbmHbJMrVBtSL4Isusu7aN1lJ25bMPlS2jefJE='
 
-
-    # FLASK_ENV=local
-    # JWT_SECRET_KEY=secretkey
-    #FERNET_KEY=b'cx9VhUbmHbJMrVBtSL4Isusu7aN1lJ25bMPlS2jefJE='
-    # NAME_DB=postgres
-    # PORT_DB=5432
-    # PWD_DB=admin
-    # HOST_DB=localhost
-    # USER_DB=postgres
-    # AWS_ACCESS_KEY=AKIARE3TT7TWTAWJC6ID
-    # AWS_SECRET_KEY=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
-
-    # AUTH_PARAMS_NAME = "authcred"
-    #FERNET_ENCRYPT_FORMAT = 'utf-8'
-    #AUTH_TOKEN_EXPIRY_DAYS = 1
-    #AUTH_TOKEN_EXPIRY_SECS = 60
-    # AWS_BUCKET_NAME = "lmsmediavault"
-
-    # current_env = os.getenv('FLASK_ENV')
-    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    #FERNET_KEY = b'cx9VhUbmHbJMrVBtSL4Isusu7aN1lJ25bMPlS2jefJE='
-
-    # NAME_DB = os.environ.get('NAME_DB')
-    # PORT_DB = os.getenv('PORT_DB')
-    # PWD_DB = os.getenv('PWD_DB')
-    # HOST_DB = os.getenv('HOST_DB')
-    # USER_DB = os.getenv('USER_DB')
-
-    # AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-    # AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+    NAME_DB = os.getenv('NAME_DB','rmg_db')
+    PORT_DB = os.getenv('PORT_DB','3306')
+    PWD_DB = os.getenv('PWD_DB','SS12VuLmaNyLtl#123')
+    HOST_DB = os.getenv('HOST_DB','65.1.199.29')
+    USER_DB = os.getenv('USER_DB','lmsadmin')
+    
